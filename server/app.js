f557eb46029e87cce6a6c75c8c15ccd6aa425bcf
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-10 14:14:19 +0630

    express setup

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
