3181c9306816205723bc7215ab972d0b94abb0e4
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-10 14:39:09 +0630

    graphQL Scehma

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// const graphqlHTTP = require("express-graphql");
 
 const app = express();
 dotenv.config();
+
+// bind express with graphql
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     // pass in a schema property
+//   })
+// );
 
 const PORT = process.env.PORT || 3000;
 
