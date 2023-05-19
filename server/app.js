fe83b2431568462d2fcd1cc07114873df2ec89a3
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-19 16:53:35 +0630

    connecting to mongodb cloud

--- server/app.js
+++ server/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose")
 
 const app = express();
 dotenv.config();
+
+// connect to database
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nodejs-database.l1yfw2q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+mongoose.connection.once('open', () => {
+  console.log('connected to database')
+})
 
 // bind express with graphql
 app.use(
