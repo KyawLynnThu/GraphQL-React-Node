77902de739a7a29f2c70ffe0fe98a07519f3b78d
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-23 16:12:20 +0630

    making query from react

--- server/app.js
+++ server/app.js
@@ -2,23 +2,29 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 dotenv.config();
 
+// allow cross-origin requests
+app.use(cors());
+
 // connect to database
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nodejs-database.l1yfw2q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
-mongoose.connection.once('open', () => {
-  console.log('connected to database')
-})
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nodejs-database.l1yfw2q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 // bind express with graphql
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: true,
     // pass in a schema property
   })
 );
