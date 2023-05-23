3d409e1b04e767ec80048287cdbfd5d0ab700fc0
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-23 16:32:12 +0630

    addbook component and fetch authors list

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+
 // components
 import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
 
 // apollo client setup
 const client = new ApolloClient({
@@ -16,6 +18,7 @@ class App extends Component {
         <div id="main">
           <h1>React, Node, GraphQL</h1>
           <BookList />
+          <AddBook />
         </div>
       </ApolloProvider>
     );
