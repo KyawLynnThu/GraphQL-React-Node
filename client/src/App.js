1fa784550ade516ec0b178ea11285e4a8df6b101
Kyaw Linn Thu <67432058+KyawLynnThu@users.noreply.github.com>
2023-05-23 15:52:31 +0630

    apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+// components
+import BookList from "./components/BookList";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
         <div id="main">
           <h1>React, Node, GraphQL</h1>
-      </div>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
